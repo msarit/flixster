db58af545ceb4d21f91b7f68830b22de74f9d25a
Arit Amana <>
2018-03-17 14:01:11 -0400

    Set Section and Course Permissions

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -8,17 +9,27 @@ class Instructor::CoursesController < ApplicationController
   def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
-    redirect_to instructor_course_path(@course)
-  else
-    render :new, status: :unprocessable_entity
-  end
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized!", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
