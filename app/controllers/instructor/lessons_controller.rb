35c0b48e31da7d6166754a8639dedefd5dcbe1aa
Arit Amana <>
2018-03-17 13:47:39 -0400

    Set Permissions on Lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,20 +1,31 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
 def new
-  @section = Section.find(params[:section_id])
   @lesson = Lesson.new
 end
 
 def create
-  @section = Section.find(params[:section_id])
-  @lesson = @section.lessons.create(lesson_params)
-  redirect_to instructor_course_path(@section.course)
+  @lesson = current_section.lessons.create(lesson_params)
+  redirect_to instructor_course_path(current_section.course)
 end
 
 private
 
+def require_authorized_for_current_section
+  if current_section.course.user != current_user
+      return render plain: 'Unauthorized!', status: :unauthorized
+    end
+end
+
 def lesson_params
   params.require(:lesson).permit(:title, :subtitle)
+end
+
+helper_method :current_section
+def current_section
+  @current_section ||= Section.find(params[:section_id])
 end
 
 end
