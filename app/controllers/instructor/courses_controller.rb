76372a0316736ab6c1121cd8907369ed161896a7
Arit Amana <>
2018-03-17 07:57:49 -0400

    Add Validations to Courses

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -7,7 +7,11 @@ class Instructor::CoursesController < ApplicationController
 
   def create
     @course = current_user.courses.create(course_params)
+    if @course.valid?
     redirect_to instructor_course_path(@course)
+  else
+    render :new, status: :unprocessable_entity
+  end
   end
 
   def show
