6ee9faa92c71108a86874d24edaad7ca65dc0038
Arit Amana <>
2018-03-21 13:35:41 -0400

    Configure Instructor dashboard

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,8 @@
+class DashboardsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
