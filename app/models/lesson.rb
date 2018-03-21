ecb988b8e730eecfd432b742084a54a01032bf97
Arit Amana <>
2018-03-21 17:29:32 -0400

    Configure Next Lesson Button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,7 +3,14 @@ class Lesson < ApplicationRecord
   mount_uploader :video, VideoUploader
 
   include RankedModel
+  ranks :row_order, with_same: :section_id
 
-  ranks :row_order, with_same: :section_id 
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    if lesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end
+    return lesson
+  end
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -4,4 +4,11 @@ class Section < ApplicationRecord
 
   include RankedModel
     ranks :row_order, with_same: :course_id
+
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return section
+  end
+
+
 end
